3ba8d9bc6a70ca8e7aeda97f5c84d28c70effab6
Yash verma <yashverma9084@gmail.com>
2025-08-12 22:54:06 +0530

    commit this code

--- AND1.java
+++ AND1.java
@@ -1,5 +1,11 @@
 public class AND1 {
     public static void main(String[] args) {
-        
+        int n1= 5 ;//0101
+        int n2 = 3;//0011
+
+        System.out.println(n1&3);
+        System.out.println(5|3);
+        System.out.println(5^3);
+        System.out.println(~5);
     }
 }
