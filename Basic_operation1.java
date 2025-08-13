104f899406e97722fa78f96c0bdeb7567f5e8dc4
Yash verma <yashverma9084@gmail.com>
2025-08-13 17:03:13 +0530

    clear bit

--- Basic_operation1.java
+++ Basic_operation1.java
@@ -10,8 +10,28 @@ public class Basic_operation1 {
         }
     }
 
+    public static int iThBit(int n , int i){
+        int bitmap = 1<<i;
+        if((n&bitmap)==0){
+            return 0;
+        }else return 1;
+    }
+
+    public static int setIthBit(int n, int i ){
+        int bitMap = 1<<i;
+        return n|bitMap;
+    }
+
+    public static int  clearBit(int n, int i){
+        int bitMap = ~(1<<i);
+        return n&bitMap;
+    }
+
     public static void main(String[] args) {
         PrintNumber(6);
+        System.out.println(iThBit(7, 1));
+        System.out.println(setIthBit(10, 2));
+        System.out.println(clearBit(10, 1));
         
     }
 }
